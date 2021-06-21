a4eb4dd0c6e33a181ee2f0d03e1d506a178b53e8
Solohaga Lukas <solohagalukas@gmail.com>
2021-06-21 11:03:01 -0300

    Requerimiento 2 (not found da error)

--- src/main/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWeb.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWeb.kt
@@ -13,10 +13,31 @@ enum class CodigoHttp(val codigo: Int) {
 class Pedido(val ip: String, val url: String, val fechaHora: LocalDateTime) {
 
     fun contieneHttp() = url.startsWith("http:")
+    fun extension(): String {
+        val separarUrl = url.split(".")
+        val division = separarUrl[separarUrl.size - 2]
+        val separar = division.split("/")
+        return separar[separar.size - 1]
+    }
 }
 class Respuesta(val codigo: CodigoHttp, val body: String, val tiempo: Int, val pedido: Pedido)
 class ServidorWeb {
-    fun recibePedido(pedido: Pedido): Respuesta = if (pedido.contieneHttp())
-        Respuesta(CodigoHttp.OK, "recibido", 8, pedido) else Respuesta(CodigoHttp.NOT_IMPLEMENTED, "No recibido", 18, pedido)
+    val modulos = mutableListOf<Modulo>()
+
+    fun recibePedido(pedido: Pedido): Respuesta {
+        val modulo = buscarModulo(pedido.extension())
+
+        when {
+            !pedido.contieneHttp() -> return Respuesta(CodigoHttp.NOT_IMPLEMENTED, "No recibido", 18, pedido)
+            !hayModulo(pedido.extension()) -> return Respuesta(CodigoHttp.NOT_FOUND, "", 10, pedido)
+            else -> return Respuesta(CodigoHttp.OK, modulo.body, modulo.tiempoRespuesta, pedido)
+        }
+    }
+
+    fun buscarModulo(extension: String) = modulos.first { m -> m.extensiones.contains(extension) }
+    fun hayModulo(extension: String) = modulos.any { m -> m.extensiones.contains(extension) }
+}
+
+class Modulo(val extensiones: MutableList<String>,val body: String, val tiempoRespuesta: Int) {
 
 }
